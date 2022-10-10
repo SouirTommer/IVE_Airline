3e1e7ca0f219e169ba1ff309a289547e9197537c
Tommer <q622826@gmail.com>
2022-10-11 00:50:59 +0800

    Add forgot pwd(with fake captcha lol)

---
+++ js/forgot.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    $("#captcha").click(function(){
+
+        $("#theimage").css("display", "none");
+        $("#theimage2").css("display", "block");
+
+        });
+
+        $("#email").focus(function(){
+            $("#email-alert").css("display", "none");
+            $("#null-alert").css("display", "none");
+        });
+
+        $("#submit").click(function(){
+            var correct = true;
+
+            if ($("#email").val().length === 0){
+                $("#null-alert").css("display", "block");
+                correct = false;
+            }
+
+            if(correct == true){
+                alert("We have send an email. Please check!");
+                
+                location.href = 'resetPwd.html';
+            }
+        });
+        
+});
