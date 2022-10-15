6870f33275da3da1b78764445f65452733afa210
sakanayue <stse2122@gmail.com>
2022-10-15 12:30:37 +0800

    half way done search function you can now click serach functino and get a json data

---
+++ js/flight_search.js
@@ -0,0 +1,507 @@
+//search engine
+
+var HKG = [{
+        flight_no: "HKG001",
+        destination: "KIX",
+        date: "2022-10-20",
+        depart_time: "9:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    },
+    {
+        flight_no: "HKG002",
+        destination: "KIX",
+        date: "2022-10-20",
+        depart_time: "16:00",
+        arrival_time: "21:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "HKG003",
+        destination: "PVG",
+        date: "2022-10-20",
+        depart_time: "10:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "HKG004",
+        destination: "PVG",
+        date: "2022-10-20",
+        depart_time: "10:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "HKG005",
+        destination: "TPE",
+        date: "2022-10-20",
+        depart_time: "10:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "HKG006",
+        destination: "TPE",
+        date: "2022-10-20",
+        depart_time: "16:00",
+        arrival_time: "20:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }
+
+]
+
+var KIX = [{
+        flight_no: "KIX001",
+        destination: "HKG",
+        date: "2022-10-20",
+        depart_time: "9:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    },
+    {
+        flight_no: "KIX002",
+        destination: "HKG",
+        date: "2022-10-20",
+        depart_time: "16:00",
+        arrival_time: "21:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "KIX003",
+        destination: "PVG",
+        date: "2022-10-20",
+        depart_time: "10:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "KIX004",
+        destination: "PVG",
+        date: "2022-10-20",
+        depart_time: "16:00",
+        arrival_time: "20:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "KIX005",
+        destination: "TPE",
+        date: "2022-10-20",
+        depart_time: "10:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "KIX006",
+        destination: "TPE",
+        date: "2022-10-20",
+        depart_time: "16:00",
+        arrival_time: "20:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }
+]
+
+var TPE = [{
+        flight_no: "TPE001",
+        destination: "HKG",
+        date: "2022-10-20",
+        depart_time: "9:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    },
+    {
+        flight_no: "TPE002",
+        destination: "HKG",
+        date: "2022-10-20",
+        depart_time: "16:00",
+        arrival_time: "21:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "TPE003",
+        destination: "PVG",
+        date: "2022-10-20",
+        depart_time: "10:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "TPE004",
+        destination: "PVG",
+        date: "2022-10-20",
+        depart_time: "10:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "TPE005",
+        destination: "KIX",
+        date: "2022-10-20",
+        depart_time: "10:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "TPE006",
+        destination: "KIX",
+        date: "2022-10-20",
+        depart_time: "16:00",
+        arrival_time: "20:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }
+]
+
+var PVG = [{
+        flight_no: "PVG001",
+        destination: "HKG",
+        date: "2022-10-20",
+        depart_time: "9:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    },
+    {
+        flight_no: "PVG002",
+        destination: "HKG",
+        date: "2022-10-20",
+        depart_time: "16:00",
+        arrival_time: "21:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "PVG003",
+        destination: "TPE",
+        date: "2022-10-20",
+        depart_time: "10:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "PVG004",
+        destination: "TPE",
+        date: "2022-10-20",
+        depart_time: "10:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "PVG005",
+        destination: "KIX",
+        date: "2022-10-20",
+        depart_time: "10:00",
+        arrival_time: "14:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }, {
+        flight_no: "PVG006",
+        destination: "KIX",
+        date: "2022-10-20",
+        depart_time: "16:00",
+        arrival_time: "20:00",
+        seat_capacity: 200,
+        sold_seat: 0,
+        economy_seat: 150,
+        economy_sold_seat: 0,
+        business_seat: 40,
+        business_sold_seat: 0,
+        firstclass_seat: 10,
+        firstclass_sold_seat: 0
+    }
+]
+var search_result;
+
+function get_arrival() {
+    var airport_code = document.getElementById("to-flight-input-autocomplete").value
+    if (airport_code == "Hong Kong Intl (HKG)")
+        airport_code = "HKG";
+    if (airport_code == "Osaka Kansai International (KIX)")
+        airport_code = "KIX";
+    if (airport_code == "Taipei Taiwan Taoyuan (TPE)")
+        airport_code = "TPE";
+    if (airport_code == "Shanghai Pu Dong (PVG)")
+        airport_code = "PVG";
+    return airport_code;
+}
+
+function get_depart() {
+    var airport_code = document.getElementById("from-flight-input-autocomplete").value
+    return airport_code;
+}
+
+function get_departdate() {
+    return document.getElementById("departDate").value;
+}
+
+function search_flight() {
+    arrival_airport = get_arrival();
+    departdate = get_departdate();
+    if (get_depart() == "Hong Kong Intl (HKG)") {
+        search_result = HKG.filter((x => x.date == departdate) && (x => x.destination == arrival_airport));
+    } else if (get_depart() == "Shanghai Pu Dong (PVG)") {
+        search_result = PVG.filter((x => x.date == departdate) && (x => x.destination == arrival_airport));
+    } else if (get_depart() == "Taipei Taiwan Taoyuan (TPE)") {
+        search_result = TPE.filter((x => x.date == departdate) && (x => x.destination == arrival_airport));
+    } else if (get_depart() == "Osaka Kansai International (KIX)") {
+        search_result = KIX.filter((x => x.date == departdate) && (x => x.destination == arrival_airport));
+    }
+    var str = JSON.stringify(search_result, null, 4);
+    alert(str);
+}
+
+
+
+
+//input script
+let input = document.getElementById("from-flight-input-autocomplete");
+let input2 = document.getElementById("to-flight-input-autocomplete");
+
+$(document).ready(function () {
+    //initiate the radio button
+    document.getElementById("return").checked = true;
+
+    // Eliminate the selected airport, preventing duplcation aiport on the input of depart the arrival
+    let airport = ["Hong Kong Intl (HKG)", "Shanghai Pu Dong (PVG)", "Osaka Kansai International (KIX)",
+        "Taipei Taiwan Taoyuan (TPE)"
+    ];
+
+    $("#to-flight-input-autocomplete").keypress(function () {
+        var input = document.getElementById("from-flight-input-autocomplete").value;
+        var index = 0;
+        var airportlist = ["Hong Kong Intl (HKG)", "Shanghai Pu Dong (PVG)",
+            "Osaka Kansai International (KIX)", "Taipei Taiwan Taoyuan (TPE)"
+        ];
+        for (i of airport) {
+            if (input == i) {
+                var index = airportlist.indexOf(i);
+                airportlist.splice(index, 1);
+            }
+        }
+        $("#to-flight-input-autocomplete").autocomplete({
+            source: airportlist
+        }, {
+            autoFocus: true,
+            delay: 0
+        });
+    });
+
+    $("#from-flight-input-autocomplete").keypress(function () {
+        var input = document.getElementById("to-flight-input-autocomplete").value;
+        var index = 0;
+        var airportlist = ["Hong Kong Intl (HKG)", "Shanghai Pu Dong (PVG)",
+            "Osaka Kansai International (KIX)", "Taipei Taiwan Taoyuan (TPE)"
+        ];
+        for (i of airport) {
+            if (input == i) {
+                index = airportlist.indexOf(i);
+                airportlist.splice(index, 1);
+            }
+        }
+        $("#from-flight-input-autocomplete").autocomplete({
+            source: airportlist
+        }, {
+            autoFocus: true,
+            delay: 0
+        });
+    });
+    // end
+
+
+    //One way radio button : disable return date picker
+    $("#radio-flight input").click(function () {
+        if (document.getElementById("oneway").checked) {
+            $("#returnDate").val("")
+            $("#returnDate").prop('disabled', true);
+        } else if (document.getElementById("return").checked) {
+            $("#returnDate").val("");
+            $("#returnDate").prop('disabled', false);
+        }
+    });
+    //end
+
+    //disable past date on datepicker
+    var today = new Date().toISOString().split('T')[0];
+    document.getElementsByName("Ddatepicker")[0]
+        .setAttribute('min', today);
+    var temp = new Date();
+    var tmr = new Date(temp);
+    tmr.setDate(temp.getDate() + 1);
+    var tomorrow = tmr.toISOString().split('T')[0];
+    document.getElementsByName("Rdatepicker")[0]
+        .setAttribute('min', tomorrow);
+    // end
+
+    // when a return date is picked first, set the max selectable date in depart datepicker
+    $("#departDate").click(function () {
+        var futureblockdate = document.getElementById("returnDate").value;
+        if (futureblockdate == "") {
+            document.getElementsByName("Ddatepicker")[0]
+                .setAttribute('max', new Date(today) + 365)
+            return;
+        }
+        var fbd = new Date(futureblockdate);
+        fbd.setDate(fbd.getDate() - 1);
+        fbd = fbd.toISOString().split('T')[0];
+        document.getElementsByName("Ddatepicker")[0]
+            .setAttribute('max', fbd);
+    })
+
+    //when a depart date is picked first, set the min selectable date in return datepicker
+    $("#returnDate").click(function () {
+        var pastblockdate = document.getElementById("departDate").value;
+        if (pastblockdate == "") {
+            document.getElementsByName("Rdatepicker")[0]
+                .setAttribute('min', tomorrow)
+            return;
+        }
+        var pbd = new Date(pastblockdate);
+        pbd.setDate(pbd.getDate() + 1);
+        pbd = pbd.toISOString().split('T')[0];
+        document.getElementsByName("Rdatepicker")[0]
+            .setAttribute('min', pbd);
+    })
+
+});
